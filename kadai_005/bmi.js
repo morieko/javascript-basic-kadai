fb7e59a97b4a9fba86e0fddec8faabff8dbc1940
morieko <riezungummy@gmail.com>
2025-09-29 14:32:39 +0900

    ファイルの追加

---
+++ kadai_005/bmi.js
@@ -0,0 +1,17 @@
+let userName;
+userName = '体重';
+let userNumber;
+userNumber = 68; //
+console.log(userName); // 
+console.log(userNumber); // 
+
+userName = '身長';
+userNumber = 1.7; // 
+console.log(userName); // 
+console.log(userNumber); // 
+
+userName = '計算式';
+console.log(userName);
+// BMIの計算式: 体重(kg) / (身長(m) × 身長(m))
+let bmi = 68 / (1.7 * 1.7);
+console.log(bmi);
