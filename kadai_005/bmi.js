b5792756a7602d80f040d68692011133df7cb64a
morieko <riezungummy@gmail.com>
2025-09-30 09:26:42 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,13 @@
+
+let weight = 68;  // 
+let height = 1.7; // 
+
+console.log("体重");
+console.log(weight);
+
+console.log("身長");
+console.log(height);
+
+let bmi = weight / (height * height); // 
+console.log("BMI");
+console.log(bmi);
